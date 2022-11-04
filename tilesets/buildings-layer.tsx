6c0f9db6d4b9ec76af194763afe64df464a96bc0
sabrina-johnson <sabrina.johnson@siemens-healthineers.com>
2022-11-04 12:58:08 +0100

    tileset added

---
+++ tilesets/buildings-layer.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="buildings-layer" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <image source="Urban-landscape-files/Urban-landscape-files/layers/buildings-layer.png" width="192" height="129"/>
+</tileset>
---
+++ tilesets/scifitiles-sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="scifitiles-sheet" tilewidth="32" tileheight="32" tilecount="84" columns="14">
+ <image source="scifitiles-sheet.png" width="448" height="192"/>
+</tileset>
