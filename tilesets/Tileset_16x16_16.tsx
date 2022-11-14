049d73d799ed5f38039e7f6d59781b6fbab78f64
sabrina-johnson <sabrina.johnson@siemens-healthineers.com>
2022-11-14 13:14:34 +0100

    mapping

--- tilesets/Tileset_16x16_16.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Tileset_16x16_16" tilewidth="32" tileheight="32" tilecount="42" columns="6">
- <image source="Modern tiles_Free/Old/Tileset_16x16_16.png" width="192" height="224"/>
-</tileset>
--- tilesets/Tileset_16x16_9.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Tileset_16x16_9" tilewidth="32" tileheight="32" tilecount="25" columns="5">
- <image source="Modern tiles_Free/Old/Tileset_16x16_9.png" width="160" height="160"/>
-</tileset>
